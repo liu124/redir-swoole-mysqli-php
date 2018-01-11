1a40dee3281b0f4ca0a6e4c7a45cedca416eb935
DESKTOP-DQMKK7L\wdg003 <m15817465660@163.com>
2018-01-11 17:27:00 +0800

    修改DB类

--- DB.class.php
+++ DB.class.php
@@ -24,27 +24,30 @@ class DB
         $this->dbname = isset($dbname) ? $dbname : 'ceshi';
         $this->port = isset($port) ? $port : '3306';
         $this->charset = isset($charset) ? $charset : 'utf-8';
-        //  $this->db_connect();
-        //  $this->db_charset();
+        $this->db_connect();
+        //$this->db_charset();
     }
 
     public function db_connect()
     {
         $this->db = new mysqli($this->host, $this->username, $this->passwd, $this->dbname, $this->port) or die('连接数据库失败');
     }
-//    public function db_charset()
-//    {
-//        $this->db = query('set {$this->charset}') or die('设置编码错误');
-//    }
+
+    public function db_charset()
+    {
+        $db = $this->db;
+        $db->query('set names urf-8') or die('设置编码错误');
+    }
+
     public function db_query()
     {
         $db = $this->db;
-//        $sql = "select * from lava_goods";
-//        $result = $db->query($sql);
-//        $data = array();
-//        while ($obj = mysqli_fetch_assoc($result)) {
-//            $data[] = $obj;
-//        }
-        return $db;
+        $sql = "select * from lava_goods";
+        $result = $db->query($sql);
+        $data = array();
+        while ($obj = mysqli_fetch_assoc($result)) {
+            $data[] = $obj;
+        }
+        return $data;
     }
 }
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-  $db = new DB();
- $result = $db->db_query();
-var_dump($result);
+  include_once ('DB.class.php');
+  $db = new DB($host='127.0.0.1', $username='root', $passwd='Liu19920201', $dbname='ceshi', $port='3306', $charset='utf-8');
+  $result = $db->db_query();
+  var_dump($result);
 
 
 
