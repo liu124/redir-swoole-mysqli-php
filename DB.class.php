0cd483f1e23c45d64afd4c3c6493ec515723abae
DESKTOP-DQMKK7L\wdg003 <m15817465660@163.com>
2018-01-12 13:20:03 +0800

    ss

--- DB.class.php
+++ DB.class.php
@@ -15,8 +15,15 @@ class DB
     private $port;
     private $db;
     private $charset;
-
-    public function __construct($host, $username, $passwd, $dbname, $port, $charset)
+   /** 构造方法
+    * @param srting $host，IP地址，默认为空
+    * @param strimg $username，用户，默认为空
+    * @param strimg $passwd，密码，默认为空
+    * @param strimg $dbname，数据库名，默认为空
+    * @param strimg $port，端口，默认为空
+    * @param strimg $charset，字符，默认为空
+    */
+    public function __construct($host=null, $username=null, $passwd=null, $dbname=null, $port=null, $charset=null)
     {
         $this->host = isset($host) ? $host : '127.0.0.1';
         $this->username = isset($username) ? $username : 'root';
@@ -24,30 +31,76 @@ class DB
         $this->dbname = isset($dbname) ? $dbname : 'ceshi';
         $this->port = isset($port) ? $port : '3306';
         $this->charset = isset($charset) ? $charset : 'utf-8';
+        //调用连接数据库
         $this->db_connect();
-        //$this->db_charset();
     }
-
+     /** 连接mysql数据库
+     */
     public function db_connect()
     {
         $this->db = new mysqli($this->host, $this->username, $this->passwd, $this->dbname, $this->port) or die('连接数据库失败');
     }
-
-    public function db_charset()
+    /** 执行sql语句
+     * @return mixed，返回全部执行sql语句，失败返回错误信息
+    */
+    private function db_query($sql)
     {
         $db = $this->db;
-        $db->query('set names urf-8') or die('设置编码错误');
+        $res = $db->query($sql)or die('sql语句执行失败');
+        return $res;
     }
-
-    public function db_query()
+    /** 查询多条记录
+     * @param string $sql ,查询sql语句
+     * @return mixed，成功返回二维数组，失败返回false
+     */
+    public function db_getAll($sql)
     {
-        $db = $this->db;
-        $sql = "select * from lava_goods";
-        $result = $db->query($sql);
-        $data = array();
-        while ($obj = mysqli_fetch_assoc($result)) {
-            $data[] = $obj;
-        }
-        return $data;
+      $res = $this->db_query($sql);
+      if(mysqli_num_rows($res))
+      {
+          $list = array();
+          while($row = mysqli_fetch_assoc($res))
+          {
+             $list[] = $row;
+          }
+          return $list;
+      }
+      return false;
+    }
+    /** 查询一条记录
+     * @param string $sql，查询sql语句
+     * @return mixed，成功返回一维数组，失败返回false
+     */
+    public function db_getRow($sql)
+    {
+        $res = $this->db_query($sql);
+        return mysqli_num_rows($res) ? mysqli_fetch_assoc($res) : false;
+    }
+    /** 更新数据
+     * @param string $sql，要更新sql语句
+     * @param boolean，成功返回受影响行数，失败返回false
+     */
+    public function db_update($sql)
+    {
+        $this->db_query($sql);
+        return mysqli_affected_rows() ? mysqli_affected_rows() : false;
+    }
+    /** 删除数据
+     * @param string $sql，要删除sql语句
+     * @param boolean，成功返回受影响行数，失败返回false
+     */
+    public function db_delete($sql)
+    {
+        $this>$this->db_query($sql);
+        return mysqli_affected_rows() ? mysqli_affected_rows() : false;
+    }
+    /** 添加数据
+     * @param string $sql，要添加sql语句
+     * @param boolean，成功返回受影响行数，失败返回false
+     */
+    public function db_insert($sql)
+    {
+        $this>$this->db_query($sql);
+        return mysqli_affected_rows() ? mysqli_affected_rows() : false;
     }
 }
--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
   include_once ('DB.class.php');
-  $db = new DB($host='127.0.0.1', $username='root', $passwd='Liu19920201', $dbname='ceshi', $port='3306', $charset='utf-8');
-  $result = $db->db_query();
+ $db = new DB();
+ //$sql = "update lava_goods set goods_title='aaj' where goods_id = 1";
+ // $sql = "insert into lava_goods (goods_title,goods_time,title_id)values('fff','121331616','2')";
+ $sql = "delete from lava_goods where goods_id=1";
+ $result = $db->db_delete($sql);
   var_dump($result);
 
 
